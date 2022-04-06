3d761adebe90f72d43a98cda2fa536699ccf6102
zeleni23 <99146666+zeleni23@users.noreply.github.com>
2022-04-06 15:51:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Zaglavlje from './components/Zaglavlje';
+import GlavniSadrzaj from './components/GlavniSadrzaj';
+import Strana from './components/Strana';
+import Podnozje from './components/Podnozje';
 
-function App() {
+  function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Zaglavlje />
+      <Strana />
+      <GlavniSadrzaj />
+      <Podnozje />
     </div>
   );
 }
-
 export default App;
